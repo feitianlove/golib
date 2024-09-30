b014ffb6170462c7a4d0c4c6f7fc56c472c634eb
feitianlove <33906671+feitianlove@users.noreply.github.com>
2024-09-30 16:18:03 +0800

    modify log format

--- common/logger/text_formatter.go
+++ common/logger/text_formatter.go
@@ -4,11 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/sirupsen/logrus"
+	"path/filepath"
 	"sort"
 	"strings"
 	"time"
-	"path/filepath"
-	"github.com/sirupsen/logrus"
 )
 
 type OMFormatter struct {
@@ -107,10 +107,69 @@ func (format *MyFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	b.WriteString(" [" + strings.ToUpper(entry.Level.String()) + "] ")
 	if entry.HasCaller() {
 		fName := filepath.Base(entry.Caller.File)
+		newLog := fmt.Sprintf("[%s:%d] %s\n",
+			fName, entry.Caller.Line, entry.Message)
+		b.WriteString(newLog)
+	} else {
+		newLog := fmt.Sprintf("[%s] %s\n", entry.Level, entry.Message)
+		b.WriteString(newLog)
+	}
+	var keys []string
+	for k := range entry.Data {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	// To perform the opertion you want
+	for idx, k := range keys {
+		switch v := entry.Data[k].(type) {
+		case error:
+			b.WriteString(fmt.Sprintf("%s:%s", k, v.Error()))
+		case int64, int32, int, uint32, uint64, uint:
+			b.WriteString(fmt.Sprintf("%s:%d", k, v))
+		case string:
+			b.WriteString(fmt.Sprintf("%s:%s", k, v))
+		case float64, float32:
+			b.WriteString(fmt.Sprintf("%s:%.2f", k, v))
+		case bool:
+			b.WriteString(fmt.Sprintf("%s:%t", k, v))
+		case time.Time:
+			b.WriteString(fmt.Sprintf("%s:%s", k, v.Format("2006-02-01 15:04:05")))
+		default:
+			bs, err := json.Marshal(v)
+			if err != nil {
+				continue
+			}
+			b.WriteString(fmt.Sprintf("%s=", k))
+			b.Write(bs)
+		}
+		if idx < len(keys)-1 {
+			b.WriteByte(' ')
+		}
+	}
+	// entry.Message 就是需要打印的日志
+	if entry.Message != "" {
+		b.WriteString(fmt.Sprintf(" msg:%s\n", entry.Message))
+	} else {
+		b.WriteByte('\n')
+	}
+	return b.Bytes(), nil
+}
+
+func (format *MyFormatter) FormatWithFunc(entry *logrus.Entry) ([]byte, error) {
+	var b *bytes.Buffer
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+	b.WriteString(time.Now().Format("2006-02-01 15:04:05.9999"))
+	b.WriteString(" [" + strings.ToUpper(entry.Level.String()) + "] ")
+	if entry.HasCaller() {
+		fName := filepath.Base(entry.Caller.File)
 		newLog := fmt.Sprintf("[%s:%d %s] %s\n",
 			fName, entry.Caller.Line, entry.Caller.Function, entry.Message)
 		b.WriteString(newLog)
-	} else{
+	} else {
 		newLog := fmt.Sprintf("[%s] %s\n", entry.Level, entry.Message)
 		b.WriteString(newLog)
 	}
